99a8b1426da18d1a981b9db51836ec3e964afc9a
unknown <mjsa.sanches25@gmail.com>
2024-03-18 14:22:26 -0300

    authUser

--- src/controllers/user/AuthUserController.ts
+++ src/controllers/user/AuthUserController.ts
@@ -7,11 +7,16 @@ class AuthUserController {
         try{
             const {email, password} = req.body as {email: string, password: string};
 
+            console.log(`Received login request for email: ${email}`);
+
             const authUserService = new AuthUserService();
             const user = await authUserService.execute({email, password});
     
+            console.log(`User ${email} logged in successfully`);
+
             return res.status(200).json({ message: "User logged successfully", user});
         }catch(error){
+            console.error('An error occurred while logging user:', error);
             return res.status(500).json({ error: 'An error occurred while logging user.', details: error });
         }
     }
--- src/services/user/AuthUserService.ts
+++ src/services/user/AuthUserService.ts
@@ -11,19 +11,23 @@ class AuthUserService {
     async execute({ email, password}: AuthRequestProps) {
         const prisma = new PrismaClient();
 
+        console.log(`Looking for user with email: ${email}`);
         const user = await prisma.user.findFirst({ where: { email: email } });
 
         if (!user) {
+            console.error(`User with email: ${email} not found`);
             throw new Error("User not found");
         }
 
         if (!user.password) {
+            console.error(`Password not found for user with email: ${email}`);
             throw new Error("Password not found");
         }
 
         const passwordMatch = await compare(password, user.password);
 
         if (!passwordMatch) {
+            console.error(`Incorrect password for user with email: ${email}`);
             throw new Error("Incorrect password");
         }
 
@@ -31,6 +35,7 @@ class AuthUserService {
         const secret = process.env.JWT_SECRET;
 
         if (!secret) {
+            console.error("JWT_SECRET is not defined");
             throw new Error("JWT_SECRET is not defined");
         }
 
@@ -45,6 +50,7 @@ class AuthUserService {
                 expiresIn: '30d'
             }
         );
+        console.log(`User with email: ${email} logged in successfully`);
         return { id: user.id, name: user.name, email: user.email, token: token }
     }
 }
